e2e0c5d5f3bf788c7298574b3e9de318e30844d5
Phat Tran <PHAT@Phattv.local>
2018-02-01 00:20:26 +0700

    Split TouristVisaFees into separate component

--- components/index.js
+++ components/index.js
@@ -14,6 +14,7 @@ import Header from "./Header";
 import Content from "./Content";
 import Footer from "./Footer";
 import BlockHeader from "./BlockHeader";
+import TouristVisaFees from "./TouristVisaFees";
 
 export {
   Anchor,
@@ -28,5 +29,6 @@ export {
   Content,
   Footer,
   BlockHeader,
-  Input
+  Input,
+  TouristVisaFees,
 };
--- pages/fees.js
+++ pages/fees.js
@@ -3,13 +3,13 @@
 import * as React from 'react';
 // custom
 import {
-  Anchor,
   BlockHeader,
   Content,
   Flexbox,
   Image,
   Layout,
   Text,
+  TouristVisaFees,
 } from '../components';
 import { colors } from '../constants/ui';
 
@@ -77,216 +77,11 @@ class VisaFees extends React.Component<Props, State> {
             </Flexbox>
 
             <BlockHeader paddingTop="5" header="DETAILED PRICING TABLES" />
-            <BlockHeader header="For tourist visas to Vietnam:" />
+            <Flexbox width="100%" justifyContent="flex-start">
+              <BlockHeader header="For tourist visas to Vietnam:" />
+            </Flexbox>
 
-            <table>
-              <tr
-                style={{
-                  backgroundColor: colors.visaRed,
-                  color: colors.white,
-                  fontWeight: 'bold',
-                }}
-              >
-                <th colSpan="2">TYPE OF VISA</th>
-                <th>1 MONTH SINGLE</th>
-                <th>1 MONTH MULTIPLE</th>
-                <th>3 MONTHS SINGLE</th>
-                <th>3 MONTHS MULTIPLE</th>
-                <th>6 MONTHS MULTIPLE</th>
-                <th>1 YEAR MULTIPLE</th>
-              </tr>
-              <tr
-                style={{
-                  backgroundColor: colors.lighterGrey,
-                }}
-              >
-                <td
-                  rowSpan="14"
-                  style={{
-                    backgroundColor: colors.visaBlue,
-                    color: colors.white,
-                    fontWeight: 'bold',
-                  }}
-                >
-                  SERVICE FEE
-                </td>
-                <td>No. of Pax</td>
-                <td
-                  colSpan="7"
-                  style={{
-                    textAlign: 'center',
-                  }}
-                >
-                  NORMAL PROCESSING (2 WORKING DAYS)
-                </td>
-              </tr>
-              <tr>
-                <td>1 pax</td>
-                <td style={{ textAlign: 'center' }}>21</td>
-                <td style={{ textAlign: 'center' }}>26</td>
-                <td style={{ textAlign: 'center' }}>34</td>
-                <td style={{ textAlign: 'center' }}>55</td>
-                <td style={{ textAlign: 'center' }} rowSpan="5">
-                  N/A
-                </td>
-                <td style={{ textAlign: 'center' }} rowSpan="5">
-                  N/A
-                </td>
-              </tr>
-              <tr>
-                <td>2 pax</td>
-                <td style={{ textAlign: 'center' }}>18.25</td>
-                <td style={{ textAlign: 'center' }}>23/25</td>
-                <td style={{ textAlign: 'center' }}>30.25</td>
-                <td style={{ textAlign: 'center' }}>52.25</td>
-              </tr>
-              <tr>
-                <td>3-5 pax</td>
-                <td style={{ textAlign: 'center' }}>17.25</td>
-                <td style={{ textAlign: 'center' }}>22.25</td>
-                <td style={{ textAlign: 'center' }}>28.25</td>
-                <td style={{ textAlign: 'center' }}>46.75</td>
-              </tr>
-              <tr>
-                <td>6-9 pax</td>
-                <td style={{ textAlign: 'center' }}>15.25</td>
-                <td style={{ textAlign: 'center' }}>19.25</td>
-                <td style={{ textAlign: 'center' }}>26.25</td>
-                <td style={{ textAlign: 'center' }}>39.05</td>
-              </tr>
-              <tr>
-                <td>10+ pax</td>
-                <td style={{ textAlign: 'center' }}>14</td>
-                <td style={{ textAlign: 'center' }}>17</td>
-                <td style={{ textAlign: 'center' }}>22</td>
-                <td style={{ textAlign: 'center' }}>33</td>
-              </tr>
-              <tr
-                style={{
-                  backgroundColor: colors.lighterGrey,
-                }}
-              >
-                <td />
-                <td
-                  colSpan="7"
-                  style={{
-                    textAlign: 'center',
-                    fontWeight: 'bold',
-                  }}
-                >
-                  URGENT PROCESSING (1 WORKING DAY)
-                </td>
-              </tr>
-              <tr>
-                <td />
-                <td
-                  colSpan="7"
-                  style={{
-                    textAlign: 'center',
-                  }}
-                >
-                  Plus 10 USD/pax
-                </td>
-              </tr>
-              <tr
-                style={{
-                  backgroundColor: colors.lighterGrey,
-                }}
-              >
-                <td />
-                <td
-                  colSpan="7"
-                  style={{
-                    textAlign: 'center',
-                    fontWeight: 'bold',
-                  }}
-                >
-                  URGENT PROCESSING (1 WORKING DAY)
-                </td>
-              </tr>
-              <tr>
-                <td />
-                <td
-                  colSpan="7"
-                  style={{
-                    textAlign: 'center',
-                  }}
-                >
-                  Plus 10 USD/pax
-                </td>
-              </tr>
-              <tr
-                style={{
-                  backgroundColor: colors.lighterGrey,
-                }}
-              >
-                <td />
-                <td
-                  colSpan="7"
-                  style={{
-                    textAlign: 'center',
-                    fontWeight: 'bold',
-                  }}
-                >
-                  SUPER-URGENT PROCESSING (4 WORKING HOURS)
-                </td>
-              </tr>
-              <tr>
-                <td />
-                <td
-                  colSpan="7"
-                  style={{
-                    textAlign: 'center',
-                  }}
-                >
-                  Plus 25 USD/pax
-                </td>
-              </tr>
-              <tr
-                style={{
-                  backgroundColor: colors.lighterGrey,
-                }}
-              >
-                <td />
-                <td
-                  colSpan="7"
-                  style={{
-                    textAlign: 'center',
-                    fontWeight: 'bold',
-                  }}
-                >
-                  INSTANT PROCESSING (2 WORKING HOURS)
-                </td>
-              </tr>
-              <tr>
-                <td />
-                <td
-                  colSpan="7"
-                  style={{
-                    textAlign: 'center',
-                  }}
-                >
-                  Plus 50 USD/pax
-                </td>
-              </tr>
-              <tr
-                style={{
-                  backgroundColor: colors.visaRed,
-                  color: colors.white,
-                  fontWeight: 'bold',
-                }}
-              >
-                <td colSpan="2" style={{ backgroundColor: colors.visaBlue }}>
-                  STAMPING FEE
-                </td>
-                <td style={{ textAlign: 'center' }}>25</td>
-                <td style={{ textAlign: 'center' }}>50</td>
-                <td style={{ textAlign: 'center' }}>25</td>
-                <td style={{ textAlign: 'center' }}>50</td>
-                <td style={{ textAlign: 'center' }}>95</td>
-                <td style={{ textAlign: 'center' }}>135</td>
-              </tr>
-            </table>
+            <TouristVisaFees />
           </Flexbox>
         </Content>
 
