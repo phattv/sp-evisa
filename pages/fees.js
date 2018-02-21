163f2e835ab8aa21fe00397919349d4aa8cb031e
Phat Tran <PHAT@Phattv.local>
2018-02-21 22:18:01 +0700

    fees: add bottom line notes

--- pages/fees.js
+++ pages/fees.js
@@ -21,47 +21,54 @@ class VisaFees extends React.Component {
         <Image src="/static/images/fees-background.png" />
         <Content>
           <Flexbox width="100%" responsiveLayout column>
-            <Flexbox>
-              <Flexbox flex={1} column alignItems="flex-start">
-                <BlockHeader header="Service fee" />
-                <Flexbox alignItems="flex-start">
-                  <Image src="/static/images/fees-image.png" />
-                  <Text p paddingLeft="5">
-                    Service fee is paid online to Vietnam-visa.com in order to
-                    get the visa approval letter approved so that you can fly to
-                    Vietnam. This fee would vary depending upon your visa type,
-                    number of applicants and processing time.
-                  </Text>
+            <Flexbox column>
+              <Text bold paddingVertical={10}>
+                Before applying for Vietnam visa on arrival, you should notice
+                that when using this kind of visa, you need to pay two kinds of
+                fee.
+              </Text>
+              <Flexbox>
+                <Flexbox flex={1} column alignItems="flex-start">
+                  <BlockHeader header="Service fee" />
+                  <Flexbox alignItems="flex-start">
+                    <Image src="/static/images/fees-image.png" />
+                    <Text p paddingHorizontal={5}>
+                      Service fee is paid online to Vietnam-visa.com in order to
+                      get the visa approval letter approved so that you can fly
+                      to Vietnam. This fee would vary depending upon your visa
+                      type, number of applicants and processing time.
+                    </Text>
+                  </Flexbox>
                 </Flexbox>
-              </Flexbox>
-              <Flexbox flex={1} column alignItems="flex-start">
-                <BlockHeader header="Stamping fee" />
-                <Flexbox alignItems="flex-start">
-                  <Image src="/static/images/fees-image.png" />
-                  <Text p paddingLeft="5">
-                    Stamping fee is paid in USD in cash for each person at the
-                    landing visa office upon arrival at Vietnam airport. It is
-                    &nbsp;
-                    <Text color="visaRed" bold>
-                      25 USD
-                    </Text>{' '}
-                    for a single entry visa, &nbsp;<Text color="visaRed" bold>
-                      50 USD
-                    </Text>{' '}
-                    for a multiple entry visa valid for up to 3 months, &nbsp;<Text
-                      color="visaRed"
-                      bold
-                    >
-                      95 USD
-                    </Text>{' '}
-                    for 6 months multiple entry visa, or &nbsp;<Text
-                      color="visaRed"
-                      bold
-                    >
-                      1325 USD
-                    </Text>{' '}
-                    for 1 year multiple entry visa.
-                  </Text>
+                <Flexbox flex={1} column alignItems="flex-start">
+                  <BlockHeader header="Stamping fee" />
+                  <Flexbox alignItems="flex-start">
+                    <Image src="/static/images/fees-image.png" />
+                    <Text p paddingHorizontal={5}>
+                      Stamping fee is paid in USD in cash for each person at the
+                      landing visa office upon arrival at Vietnam airport. It is
+                      &nbsp;
+                      <Text color="visaRed" bold>
+                        25 USD
+                      </Text>{' '}
+                      for a single entry visa, &nbsp;<Text color="visaRed" bold>
+                        50 USD
+                      </Text>{' '}
+                      for a multiple entry visa valid for up to 3 months, &nbsp;<Text
+                        color="visaRed"
+                        bold
+                      >
+                        95 USD
+                      </Text>{' '}
+                      for 6 months multiple entry visa, or &nbsp;<Text
+                        color="visaRed"
+                        bold
+                      >
+                        1325 USD
+                      </Text>{' '}
+                      for 1 year multiple entry visa.
+                    </Text>
+                  </Flexbox>
                 </Flexbox>
               </Flexbox>
             </Flexbox>
@@ -75,13 +82,20 @@ class VisaFees extends React.Component {
             </Flexbox>
             <TouristVisaFees />
 
-            <Flexbox width="100%" justifyContent="flex-start" paddingTop="8">
+            <Flexbox width="100%" juchrstifyContent="flex-start" paddingTop="8">
               <BlockHeader
                 header="For business visas to Vietnam:"
                 smallPadding
               />
             </Flexbox>
             <BusinessVisaFees />
+
+            <Flexbox width="100%" justifyContent="flex-start" paddingTop="5">
+              <Text bold>
+                * Please note that service fee for business visas includes
+                Fast-track service
+              </Text>
+            </Flexbox>
           </Flexbox>
         </Content>
 
