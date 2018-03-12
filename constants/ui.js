a97ca3b5a24cae3f6c00f859227cf48a77d9d51b
Phat Tran <PHAT@Phattv.local>
2018-03-13 00:56:34 +0700

    Use named export for ui.js

--- constants/ui.js
+++ constants/ui.js
@@ -1,42 +1,57 @@
 // @flow
-export const screenSizes = {
+const screenSizes = {
   desktop: 1440 - 1,
   tablet: 768 - 1,
-  mobile: 425 - 1
+  mobile: 425 - 1,
 };
 
-export const spacingUnits = {
+const spacingUnits = {
   desktop: 5,
   tablet: 4,
-  mobile: 3
+  mobile: 3,
 };
-export const borderRadius = 8;
 
-export const colors = {
-  visaRed: "#CD2122",
-  darkRed: "#9F0F12",
-  visaBlue: "#0061B1",
-  darkBlue: "#003058",
-  lightBlue: "#F0F5F8",
-  darkGrey: "#3E3E3E",
-  grey: "#626262",
-  lightGrey: "#D6D9DF",
+const borderRadius = 8;
+
+const colors = {
+  visaRed: '#CD2122',
+  darkRed: '#9F0F12',
+  visaBlue: '#0061B1',
+  darkBlue: '#003058',
+  lightBlue: '#F0F5F8',
+  darkGrey: '#3E3E3E',
+  grey: '#626262',
+  lightGrey: '#D6D9DF',
   lighterGrey: '#EFEFEF',
-  white: "#FFFFFF"
+  white: '#FFFFFF',
 };
 
-export const fontSizes = {
+const fontSizes = {
   xs: 12,
   s: 14,
   default: 16,
   m: 18,
   l: 22,
   xl: 32,
-  xxl: 42
+  xxl: 42,
 };
 
-export const pageNames = {
-  generic: "generic",
-  property: "property",
-  jobs: "jobs"
+const spacingValues = {
+  xxs: 4,
+  xs: 8,
+  s: 12,
+  m: 16,
+  l: 20,
+  xl: 24,
+  xxl: 28,
+  xxxl: 32,
+};
+
+export {
+  screenSizes,
+  spacingUnits,
+  borderRadius,
+  colors,
+  fontSizes,
+  spacingValues,
 };
