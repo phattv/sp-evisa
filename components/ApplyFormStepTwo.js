6e70e2af016384212bff5f5d06ce2ad288d196de
Phat Tran <PHAT@Phattv.local>
2018-03-18 13:05:09 +0700

    WIP: step two

--- components/ApplyFormStepTwo.js
+++ components/ApplyFormStepTwo.js
@@ -1,8 +1,9 @@
 // @flow
 // vendor
 import * as React from 'react';
+import { Div } from 'glamorous'
 // custom
-
+import { Flexbox } from '../components'
 
 type Props = {};
 type State = {};
@@ -17,7 +18,14 @@ class ApplyFormStepTwo extends React.Component<Props, State> {
   state = {};
 
   render() {
-    return <div>Please fill out all required information below to process Vietnam visa approval letter right now OR just select applicant's nationality, then SKIP THIS STEP. An application form will be sent to your registered email for later fulfilling.</div>;
+    return (
+      <Div>
+        Please fill out all required information below to process Vietnam visa
+        approval letter right now OR just select applicant's nationality, then
+        SKIP THIS STEP. An application form will be sent to your registered
+        email for later fulfilling.
+      </Div>
+    );
   }
 }
 
