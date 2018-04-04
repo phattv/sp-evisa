3a13e6e664fae5e3164a98eb22f228c8e7d0bdc1
Phat Tran <PHAT@Phattv.local>
2018-04-05 02:25:42 +0700

    Export all static pages

--- next.config.js
+++ next.config.js
@@ -1,18 +1,21 @@
 // https://github.com/zeit/next.js#custom-configuration
 // Configuration object for "next build" & "next export"
 
-// // "yarn dev" doesn't run .babel, replicate logic from env-config.js here
-// const isProd =
-//   process.env.NODE_ENV === 'production' || process.env.NODE_ENV === 'prod'
-//
-// const assetPrefix = isProd ? '/pro' : ''
-// console.log('next.config.js - assetPrefix:', assetPrefix)
-
 module.exports = {
   exportPathMap: function() {
     return {
-      "/": { page: "/" }
-      // '/pages/about.html': { page: '/about' },
+      "/": { page: "/" },
+      "/about": { page: "/about" },
+      "/apply": { page: "/apply" },
+      "/contact": { page: "/contact" },
+      "/feedback": { page: "/feedback" },
+      "/fees": { page: "/fees" },
+      "/how": { page: "/how" },
+      "/news": { page: "/news" },
+      "/partners": { page: "/partners" },
+      "/privacy": { page: "/privacy" },
+      "/services": { page: "/services" },
+      "/terms": { page: "/terms" },
     };
   }
 };
