504545189b02e1989c320992858bfe71821bf3c7
Phat Tran <PHAT@Phattv.local>
2018-04-11 02:56:18 +0700

    Add /register & /login APIs

---
+++ pages/login.js
@@ -0,0 +1,161 @@
+// @flow
+// vendor
+import React from 'react';
+import { Input } from 'glamorous';
+import Router from 'next/router';
+// custom
+import {
+  Anchor,
+  BlockHeader,
+  Button,
+  Content,
+  Flexbox,
+  Layout,
+  Text,
+} from '../components';
+import { borderRadius, colors, spacingValues } from '../constants/ui';
+import { login } from '../utils/apiClient';
+
+type Props = {};
+type State = {
+  email: string,
+  password: string,
+  errorMessage: string,
+};
+class Login extends React.Component<Props, State> {
+  state = {
+    email: '',
+    password: '',
+    errorMessage: '',
+  };
+
+  componentDidMount() {
+    window.Intercom('update');
+  }
+
+  updateEmail = (event: Object) => {
+    this.setState({
+      email: event.target.value,
+    });
+  };
+
+  updatePassword = (event: Object) => {
+    this.setState({
+      password: event.target.value,
+    });
+  };
+
+  login = () => {
+    this.setState({
+      errorMessage: '',
+    });
+
+    const { email, password } = this.state;
+    login(
+      {
+        email,
+        password,
+      },
+      response => {
+        if (response.error) {
+          return this.setState({
+            errorMessage: response.message,
+          });
+        } else {
+          this.setState({
+            errorMessage: '',
+          });
+          Router.push('/').then(() => window.scrollTo(0, 0));
+        }
+      },
+    );
+  };
+
+  render() {
+    const { email, password, errorMessage } = this.state;
+    return (
+      <Layout>
+        <Content>
+          <Flexbox width="100%">
+            <Flexbox
+              flex={1}
+              width="100%"
+              maxWidth={100}
+              column
+              border
+              borderRadius
+              paddingVertical={5}
+              paddingHorizontal={5}
+            >
+              <BlockHeader header="Login" />
+
+              <Flexbox
+                alignItems="flex-start"
+                paddingBottom={3}
+                column
+                width="100%"
+              >
+                <Text bold>Email</Text>
+                <Input
+                  type="email"
+                  name="username"
+                  backgroundColor="white"
+                  padding={`${spacingValues.xs}px ${spacingValues.s}px`}
+                  borderRadius={borderRadius}
+                  border={`1px solid ${colors.lightGrey}`}
+                  width="100%"
+                  value={email}
+                  onChange={this.updateEmail}
+                  marginTop={2}
+                />
+              </Flexbox>
+
+              <Flexbox
+                alignItems="flex-start"
+                paddingBottom={3}
+                column
+                width="100%"
+              >
+                <Text bold>Password</Text>
+                <Input
+                  type="password"
+                  name="password"
+                  backgroundColor="white"
+                  padding={`${spacingValues.xs}px ${spacingValues.s}px`}
+                  borderRadius={borderRadius}
+                  border={`1px solid ${colors.lightGrey}`}
+                  width="100%"
+                  value={password}
+                  onChange={this.updatePassword}
+                  marginTop={2}
+                />
+              </Flexbox>
+
+              <Flexbox
+                justifyContent="flex-end"
+                paddingBottom={10}
+                width="100%"
+              >
+                <Anchor href="/register">Register</Anchor>
+                &nbsp;&nbsp;|&nbsp;&nbsp;
+                <Anchor href="/forgot">Forgot password</Anchor>
+              </Flexbox>
+
+              {errorMessage && (
+                <Text color="visaRed" p>
+                  {errorMessage}
+                </Text>
+              )}
+
+              <Button solid onClick={this.login}>
+                LOGIN
+              </Button>
+            </Flexbox>
+          </Flexbox>
+        </Content>
+      </Layout>
+    );
+  }
+}
+
+export default Login;
--- utils/apiClient.js
+++ utils/apiClient.js
@@ -4,19 +4,44 @@ import axios from 'axios';
 // custom
 
 const getFeesByCountryId = (params: Object, callback: Function) => {
-  const isProd = location.hostname.endsWith('evisa-vn.com');
-  const apiServer = isProd
-    ? 'http://api.evisa-vn.com'
-    : 'http://localhost:8001';
-
   return axios
-    .get(`${apiServer}/fees-by-country/${params.countryId}`)
+    .get(`${getApiServer()}/fees-by-country/${params.countryId}`)
     .then(response => {
       return callback(response.data);
     })
     .catch(error => {
       console.error('xxx', error);
+      return callback(error.response.data);
     });
 };
 
-export { getFeesByCountryId };
+const login = (params: Object, callback: Function) => {
+  return axios
+    .post(`${getApiServer()}/login`, params)
+    .then(response => {
+      return callback(response.data);
+    })
+    .catch(error => {
+      console.error('xxx', error);
+      return callback(error.response.data);
+    });
+};
+
+const register = (params: Object, callback: Function) => {
+  return axios
+    .post(`${getApiServer()}/register`, params)
+    .then(response => {
+      return callback(response.data);
+    })
+    .catch(error => {
+      console.error('xxx', error);
+      return callback(error.response.data);
+    });
+};
+
+const getApiServer = () =>
+  location.hostname.endsWith('evisa-vn.com')
+    ? 'http://api.evisa-vn.com'
+    : 'http://localhost:8001';
+
+export { getFeesByCountryId, login, register };
