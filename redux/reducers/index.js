1e12d8f9ad9b9f0819bdac17a33bcaabd44a8f71
Phat Tran <PHAT@Phattv.local>
2018-04-11 03:04:34 +0700

    Rename redux/reducers/reducers.js to index.js

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -4,13 +4,15 @@ import { combineReducers } from 'redux';
 // custom
 import { form } from './form';
 import { fees } from './fees';
+import { account } from './account';
 import { reducerNames } from '../../constants/reducerNames';
 
 const reducers = {
   [reducerNames.form]: form,
   [reducerNames.fees]: fees,
+  [reducerNames.account]: account,
 };
-const rootReducer = combineReducers({ ...reducers });
+const rootReducer = combineReducers(reducers);
 
 /**
  * +---------+
