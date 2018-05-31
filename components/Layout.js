caf201fbc66dbf130de13da1a6bc97063477772a
Phat Tran <PHAT@Phattv.local>
2018-05-31 09:44:54 +0700

    Update title & meta tags

--- components/Layout.js
+++ components/Layout.js
@@ -33,9 +33,14 @@ export default class Layout extends React.Component<Props> {
     return (
       <Flexbox {...rest}>
         <Head>
-          <title>{title || 'Vietam visa'}</title>
+          <title>{title || 'Trusted & Instant Vietnam Visa Service'}</title>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
           <meta charSet="utf-8" />
+          <meta name="application-name" content="evisa-vn.com" />
+          <meta name="theme-color" content="#0061B1" />
+          <meta name="description" content="Trusted and instant Vietnam visa services best price guarantee. 24/7 customer support" />
+          <meta name="keywords" content="vietnam, visa, best, price, guarantee, trusted, fast, services, service, urgent, 30, minutes, mins, convenient, fees, fee"/>
+
           <link href="../static/styles/sanitize.css" rel="stylesheet" />
           <link href="../static/styles/react-select.css" rel="stylesheet" />
           <link href="../static/styles/font-awesome.min.css" rel="stylesheet" />
