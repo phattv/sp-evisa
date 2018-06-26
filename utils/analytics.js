9c424059547ecddad1bab03a7853cc18ce0379db
Phat Tran <PHAT@Phattv.local>
2018-06-27 00:15:50 +0700

    Add timeout of 100ms for logPageView

--- utils/analytics.js
+++ utils/analytics.js
@@ -2,28 +2,36 @@
 // @flow
 import ReactGA from 'react-ga';
 
-export const initGA = () => {
+const initGA = () => {
   console.log('GA init');
   ReactGA.initialize('UA-116667894-1');
 };
 
-export const logPageView = () => {
-  console.log(`Logging pageview for ${window.location.pathname}`);
-  ReactGA.set({ page: window.location.pathname });
-  ReactGA.pageview(window.location.pathname);
+const logPageView = () => {
+  const logPageViewInternal = () => {
+    console.log(`Logging pageview for ${window.location.pathname}`);
+    ReactGA.set({ page: window.location.pathname });
+    ReactGA.pageview(window.location.pathname);
+  };
+
+  // Wait for GA_INITIALIZED
+  if (!window.GA_INITIALIZED) {
+    setTimeout(logPageViewInternal, 100);
+  } else {
+    logPageViewInternal();
+  }
 };
 
-export const logEvent = (category: string = '', action: string = '') => {
+const logEvent = (category: string = '', action: string = '') => {
   if (category && action) {
     ReactGA.event({ category, action });
   }
 };
 
-export const logException = (
-  description: string = '',
-  fatal: boolean = false,
-) => {
+const logException = (description: string = '', fatal: boolean = false) => {
   if (description) {
     ReactGA.exception({ description, fatal });
   }
 };
+
+export { initGA, logPageView, logEvent, logException };
