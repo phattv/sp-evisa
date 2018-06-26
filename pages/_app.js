e352d8b4d4dd8691e551bf71618edd7ae5f2216f
Phat Tran <PHAT@Phattv.local>
2018-06-27 00:15:14 +0700

    Load third-party scripts & global styles in _app.js, load Rubik fonts in _document.js

--- pages/_app.js
+++ pages/_app.js
@@ -3,13 +3,31 @@
 import React from 'react';
 import App, { Container } from 'next/app';
 // custom
-import Layout from '../components/Layout'
+import Layout from '../components/Layout';
+import { initGA } from '../utils/analytics';
 
 export default class MyApp extends App {
-  componentDidCatch (error, errorInfo) {
-    console.log('CUSTOM ERROR HANDLING', error)
+  componentDidCatch(error, errorInfo) {
+    console.log('CUSTOM ERROR HANDLING', error);
     // This is needed to render errors correctly in development / production
-    super.componentDidCatch(error, errorInfo)
+    super.componentDidCatch(error, errorInfo);
+  }
+
+  componentDidMount() {
+    /**
+     * Load third-party services on production mode only
+     * - Google Analytics
+     * - Crisp (chat)
+     * - Rollbar (error tracking)
+     */
+    if (process.env.NODE_ENV === 'production') {
+      if (!window.GA_INITIALIZED) {
+        initGA();
+        window.GA_INITIALIZED = true;
+      }
+      require('../static/crisp');
+      require('../static/rollbar');
+    }
   }
 
   render() {
@@ -18,11 +36,12 @@ export default class MyApp extends App {
       <Container>
         <Layout>
           <style jsx global>{`
-body {
-  font-family: 'Rubik', -apple-system, system-ui, BlinkMacSystemFont, 'SegoeUI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif';
-  font-size: 16px;
-}
-`}</style>
+            body {
+              font-family: 'Rubik', -apple-system, system-ui, BlinkMacSystemFont,
+                'SegoeUI', 'Roboto', 'Helvetica Neue', 'Arial', 'sans-serif';
+              font-size: 16px;
+            }
+          `}</style>
           <Component {...pageProps} />
         </Layout>
       </Container>
--- pages/_document.js
+++ pages/_document.js
@@ -1,39 +1,50 @@
 // @flow
 // vendor
-import Document, { Head, Main, NextScript } from 'next/document'
-import { ServerStyleSheet } from 'styled-components'
+import Document, { Head, Main, NextScript } from 'next/document';
+import { ServerStyleSheet } from 'styled-components';
 // custom
-import { initGA, logPageView } from '../utils/analytics';
 
 export default class MyDocument extends Document {
-  static getInitialProps ({ renderPage }) {
-    const sheet = new ServerStyleSheet()
-    const page = renderPage(App => props => sheet.collectStyles(<App {...props} />))
-    const styleTags = sheet.getStyleElement()
-    return { ...page, styleTags }
+  static getInitialProps({ renderPage }) {
+    const sheet = new ServerStyleSheet();
+    const page = renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />),
+    );
+    const styleTags = sheet.getStyleElement();
+    return { ...page, styleTags };
   }
 
-  render () {
+  render() {
     return (
       <html>
-      <Head>
-        <title>evisa-vn - Trusted & Instant Vietnam Visa Service</title>
-        {this.props.styleTags}
+        <Head>
+          {/* TODO: dynamic title */}
+          <title>evisa-vn - Trusted & Instant Vietnam Visa Service</title>
+          {this.props.styleTags}
 
-        <meta charSet="utf-8" />
-        <meta name="application-name" content="evisa-vn.com" />
-        <meta name="theme-color" content="#0061B1" />
-        <meta name="description" content="Trusted and instant Vietnam visa services best price guarantee. 24/7 customer support" />
-        <meta name="keywords" content="vietnam, visa, best, price, guarantee, trusted, fast, services, service, urgent, 30, minutes, mins, convenient, fees, fee"/>
+          <meta charSet="utf-8" />
+          <meta name="application-name" content="evisa-vn.com" />
+          <meta name="theme-color" content="#0061B1" />
+          <meta
+            name="description"
+            content="Trusted and instant Vietnam visa services best price guarantee. 24/7 customer support"
+          />
+          <meta
+            name="keywords"
+            content="vietnam, visa, best, price, guarantee, trusted, fast, services, service, urgent, 30, minutes, mins, convenient, fees, fee"
+          />
 
-        <link href="../static/styles/sanitize.css" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css?family=Rubik" rel="stylesheet"/>
-      </Head>
-      <body>
-      <Main />
-      <NextScript />
-      </body>
+          <link href="../static/styles/sanitize.css" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css?family=Rubik"
+            rel="stylesheet"
+          />
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
       </html>
-    )
+    );
   }
 }
