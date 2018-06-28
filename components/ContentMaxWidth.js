557571729ef1d34ed59c2e3d70922266cb8313dc
Phat Tran <PHAT@Phattv.local>
2018-06-29 00:38:38 +0700

    Merge FullWidthBackgroundContainer into ContentMaxWidth

--- components/ContentMaxWidth.js
+++ components/ContentMaxWidth.js
@@ -7,13 +7,31 @@ import { Flexbox } from './ui';
 import { contentMaxWidth } from '../constants/ui';
 
 type Props = {
+  backgroundImage?: string,
   children: Node,
 };
 type State = {};
 class ContentMaxWidth extends React.Component<Props, State> {
   render() {
-    const { children } = this.props;
-    return (
+    const { backgroundImage, children } = this.props;
+    return backgroundImage ? (
+      <Flexbox
+        width="100%"
+        style={{
+          backgroundImage,
+          backgroundSize: 'cover',
+          backgroundRepeat: 'no-repeat',
+        }}
+      >
+        <Flexbox
+          width={'100%'}
+          maxWidth={contentMaxWidth}
+          style={{ margin: 'auto' }}
+        >
+          {children}
+        </Flexbox>
+      </Flexbox>
+    ) : (
       <Flexbox
         width={'100%'}
         maxWidth={contentMaxWidth}
