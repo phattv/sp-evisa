7b8ff65cb39f015bd8e864a53b9cb746d23f3cdb
Phat Tran <PHAT@Phattv.local>
2018-07-03 01:39:57 +0700

    Add ServicesCard component

---
+++ components/ServicesCard.js
@@ -0,0 +1,77 @@
+// @flow
+// vendor
+import React from 'react';
+import Router from 'next/router';
+// custom
+import { Flexbox, Image } from './ui';
+import ContentMaxWidth from './ContentMaxWidth';
+import Heading from './Heading';
+import { pageNames } from '../constants/ui';
+
+const services = [
+  {
+    image: 'fast-track.jpg',
+    text: 'Airport fast track',
+  },
+  {
+    image: 'pick-up.jpg',
+    text: 'Pick up at airport',
+  },
+  {
+    image: 'private-letter.jpg',
+    text: 'Private letter',
+  },
+];
+
+/**
+ * ServicesCard show services as Card layout with a background image
+ */
+type Props = {};
+type State = {};
+class ServicesCard extends React.Component<Props, State> {
+  navigateToServices = () => Router.push(pageNames.services);
+
+  render() {
+    return (
+      <ContentMaxWidth>
+        <Flexbox column width="100%" alignItems="center">
+          <Heading text="Other services" />
+          <Flexbox
+            width="100%"
+            justifyContent="center"
+            alignItems="center"
+            paddingTop={6}
+            responsiveLayout
+          >
+            {services.map((service, index) => (
+              <Flexbox
+                key={index}
+                flex={1}
+                clickable
+                column
+                onClick={this.navigateToServices}
+                maxWidth={60}
+                marginHorizontal={2}
+                marginVertical={2}
+              >
+                <Image
+                  src={`../static/images/${service.image}`}
+                  alt={service.image}
+                />
+                <Flexbox
+                  backgroundColor="white"
+                  paddingHorizontal={3}
+                  paddingVertical={3}
+                >
+                  {service.text}
+                </Flexbox>
+              </Flexbox>
+            ))}
+          </Flexbox>
+        </Flexbox>
+      </ContentMaxWidth>
+    );
+  }
+}
+
+export default ServicesCard;
