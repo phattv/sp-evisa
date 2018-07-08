3c4f743198431915389caa6b1884711bddb89751
Phat Tran <PHAT@Phattv.local>
2018-07-08 23:12:18 +0700

    Update index page, add simple fees calculator

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,9 @@
 import React, { Fragment } from 'react';
 import { logPageView } from '../utils/analytics';
 import Router from 'next/router';
+import { Form, Dropdown } from 'semantic-ui-react';
+import _get from 'lodash/get';
+import { connect } from 'react-redux';
 // custom
 import { Button, Flexbox, Image, Text } from '../components/ui';
 import ContentMaxWidth from '../components/ContentMaxWidth';
@@ -17,24 +20,206 @@ import {
   paddingAll,
   pageNames,
   spacingValues,
+  borderRadius,
 } from '../constants/ui';
+import {
+  airportFastTrackOptions,
+  countryOptions,
+  processingTimeOptions,
+  purposeOptions,
+  typeOptions,
+} from '../constants/dropDownOptions';
+import {
+  updateFees,
+  updateFeesSelectedCountry,
+  updateStepOne,
+} from '../redux/actions';
+import { getFeesByCountryId } from '../utils/apiClient';
+import { fees } from '../constants/fees';
+import { reducerNames } from '../constants/reducerNames';
 
-type Props = {};
-type State = {};
+type Props = {
+  countryId: number,
+  stepOne: Object,
+  updateStepOne: Object => void,
+  updateFees: (Array<Object>) => void,
+  updateFeesSelectedCountry: number => void,
+  fees: Array<Object>,
+};
+type State = {
+  countryId: number | string,
+  purpose: string,
+  type: string,
+  processingTime: string,
+  extraServices: Object,
+  totalFee: number,
+};
 class Home extends React.Component<Props, State> {
-  componentDidMount() {
-    logPageView();
-  }
+  state = {
+    countryId: '',
+    purpose: purposeOptions[0].value,
+    type: typeOptions[0].value,
+    processingTime: processingTimeOptions[0].value,
+    extraServices: {
+      fastTrack: '',
+      carPickup: '',
+      privateVisaLetter: false,
+    },
+    totalFee: 0,
+  };
 
-  navigateApplyPage = () => {
+  navigateToApply = () => {
     Router.push(pageNames.apply);
   };
 
+  //<editor-fold desc="TODO: repeated code & logic between this & ApplyFormStepOne">
+  updateCountryId = (event: Object, selectedOption: Object) => {
+    if (selectedOption) {
+      this.setState(
+        {
+          countryId: selectedOption.value,
+        },
+        () => {
+          this.updateStepOneToStore();
+          this.props.updateFeesSelectedCountry(selectedOption.value);
+          getFeesByCountryId(
+            { countryId: selectedOption.value },
+            this.updateFees,
+          );
+        },
+      );
+    } else {
+      this.setState({
+        countryId: 0,
+      });
+    }
+  };
+
+  updateFees = data => {
+    this.props.updateFees(data);
+  };
+
+  updatePurpose = (event: Object, selectedOption: Object) => {
+    return this.setState(
+      {
+        purpose: selectedOption ? selectedOption.value : '',
+      },
+      () => this.updateStepOneToStore(),
+    );
+  };
+
+  updateType = (event: Object, selectedOption: Object) =>
+    this.setState(
+      {
+        type: selectedOption ? selectedOption.value : '',
+      },
+      () => this.updateStepOneToStore(),
+    );
+
+  updateProcessingTime = (event: Object, selectedOption: Object) => {
+    // Logic note: if choose emergency, fast track is chosen
+    this.setState(
+      {
+        processingTime: selectedOption ? selectedOption.value : '',
+        extraServices: {
+          ...this.state.extraServices,
+          fastTrack:
+            selectedOption.value === 'emergency'
+              ? airportFastTrackOptions[1].value
+              : '',
+        },
+      },
+      () => this.updateStepOneToStore(),
+    );
+  };
+
+  updateStepOneToStore = () => {
+    this.props.updateStepOne({
+      countryId: this.state.countryId,
+      purpose: this.state.purpose,
+      type: this.state.type,
+      processingTime: this.state.processingTime,
+      extraServices: this.state.extraServices,
+    });
+  };
+
+  componentDidMount() {
+    this.syncPropsToState(this.props, true);
+    logPageView();
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.stepOne !== nextProps.stepOne) {
+      this.syncPropsToState(nextProps);
+    }
+  }
+
+  syncPropsToState = (nextProps: Props, isForced?: boolean) => {
+    if (isForced || this.props.stepOne !== nextProps.stepOne) {
+      this.setState({
+        countryId: _get(nextProps, 'stepOne.countryId', ''),
+        type: _get(nextProps, 'stepOne.type', typeOptions[0].value),
+        purpose: _get(nextProps, 'stepOne.purpose', purposeOptions[0].value),
+        processingTime: _get(
+          nextProps,
+          'stepOne.processingTime',
+          processingTimeOptions[0].value,
+        ),
+        extraServices: _get(nextProps, 'stepOne.extraServices', {}),
+      });
+    }
+
+    if (isForced || this.props.countryId !== nextProps.countryId) {
+      this.setState({
+        countryId: nextProps.countryId,
+      });
+    }
+
+    this.calculateTotalFee();
+  };
+  //</editor-fold>
+
+  calculateTotalFee = () => {
+    const { purpose, type, processingTime, extraServices } = this.state;
+
+    const fees = _get(this, 'props.fees', []).find(
+      fees => fees.type === purpose,
+    );
+    const processingTimeObject = processingTimeOptions.find(
+      option => option.value === processingTime,
+    );
+    const fastTrackObject = airportFastTrackOptions.find(
+      option => option.value === extraServices.fastTrack,
+    );
+
+    const costPerPerson = fees && fees[type] > 0 ? fees[type] : 0;
+    const processingFees = _get(processingTimeObject, 'price', 0);
+    const extraFees = _get(fastTrackObject, 'price', 0);
+
+    const totalFee = costPerPerson + processingFees + extraFees;
+
+    this.setState(
+      {
+        totalFee,
+      },
+      // () => this.props.updatePrice(totalFee),
+    );
+  };
+
   render() {
+    const { countryId, type, processingTime, purpose, totalFee } = this.state;
+
+    let typeOptionsByPurpose = [];
+    if (purpose === purposeOptions[0].value) {
+      typeOptionsByPurpose = typeOptions.slice(0, 4);
+    } else {
+      typeOptionsByPurpose = typeOptions;
+    }
+
     return (
       <Fragment>
         <ContentMaxWidth backgroundImage="url('../static/images/bg-banner.jpg')">
-          <Flexbox paddingTop={spacingValues.blockPaddingTop} responsiveLayout>
+          <Flexbox paddingVertical={10} responsiveLayout alignItems="center">
             <Flexbox flex={1} column {...paddingAll}>
               <Heading
                 color="white"
@@ -48,13 +233,90 @@ class Home extends React.Component<Props, State> {
                 visa in just 3 simple steps with evisa-vn, so you would have
                 more time planning your trips.
               </Text>
-              <Button onClick={this.navigateApplyPage}>Apply Now!</Button>
             </Flexbox>
-            <Flexbox flex={1} column paddingHorizontal={5} paddingTop={5}>
-              <Image
-                src="../static/images/right-banner.png"
-                alt="right banner"
-              />
+            <Flexbox
+              flex={1}
+              column
+              backgroundColor="bgGrey2"
+              borderRadius={borderRadius}
+              overflow="hidden"
+            >
+              <Form
+                onSubmit={this.navigateToApply}
+                style={{
+                  width: '100%',
+                }}
+              >
+                <Flexbox {...paddingAll} column>
+                  <Text fontSize="m" textAlign="center">
+                    How much does it cost?
+                  </Text>
+                  <Form.Field required>
+                    <label>Country</label>
+                    <Dropdown
+                      value={countryId}
+                      placeholder="Select..."
+                      fluid
+                      search
+                      selection
+                      options={countryOptions}
+                      onChange={this.updateCountryId}
+                    />
+                  </Form.Field>
+                  <Form.Field required>
+                    <label>Purpose of Visa</label>
+                    <Dropdown
+                      value={purpose}
+                      placeholder="Select..."
+                      fluid
+                      search
+                      selection
+                      options={purposeOptions}
+                      onChange={this.updatePurpose}
+                    />
+                  </Form.Field>
+                  <Form.Field required>
+                    <label>Type of Visa</label>
+                    <Dropdown
+                      value={type}
+                      placeholder="Select..."
+                      fluid
+                      search
+                      selection
+                      options={typeOptionsByPurpose}
+                      onChange={this.updateType}
+                    />
+                  </Form.Field>
+                  <Form.Field required>
+                    <label>Processing Time</label>
+                    <Dropdown
+                      value={processingTime}
+                      placeholder="Select..."
+                      fluid
+                      search
+                      selection
+                      options={processingTimeOptions}
+                      onChange={this.updateProcessingTime}
+                    />
+                  </Form.Field>
+
+                  <Flexbox
+                    backgroundColor="darkBlue"
+                    paddingHorizontal={3}
+                    paddingVertical={2}
+                    justifyContent="space-between"
+                  >
+                    <Text color="white">Total Fee</Text>
+                    <Text bold color="white">
+                      ${totalFee} / Person
+                    </Text>
+                  </Flexbox>
+                </Flexbox>
+
+                <Button type="submit" width="100%" borderRadius={0}>
+                  Apply Now!
+                </Button>
+              </Form>
             </Flexbox>
           </Flexbox>
         </ContentMaxWidth>
@@ -215,5 +477,16 @@ class Home extends React.Component<Props, State> {
     );
   }
 }
-
-export default Home;
+const mapStateToProps = store => {
+  return {
+    countryId: _get(store, 'fees.countryId', null),
+    stepOne: _get(store, 'form.stepOne', {}),
+    fees: store[reducerNames.fees].fees,
+  };
+};
+const mapDispatchToProps = {
+  updateStepOne,
+  updateFees,
+  updateFeesSelectedCountry,
+};
+export default connect(mapStateToProps, mapDispatchToProps)(Home);
