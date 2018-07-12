68bd22e36907b32c9fb70f38bfede6b68e110b1a
Phat Tran <PHAT@Phattv.local>
2018-07-13 02:43:24 +0700

    Add withCss & webpack file-loader to next.config.js to load static files

--- next.config.js
+++ next.config.js
@@ -1,8 +1,9 @@
+const withCss = require('@zeit/next-css');
 const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const { ANALYZE } = process.env;
 
-module.exports = {
-  webpack: function(config, { isServer }) {
+module.exports = withCss({
+  webpack: function(config, { dev, isServer }) {
     // Read ANALYZE env variable from package.json script "analyze" and show report
     if (ANALYZE) {
       config.plugins.push(
@@ -14,6 +15,19 @@ module.exports = {
       );
     }
 
+    config.module.rules.push({
+      test: /\.(png|svg|eot|otf|ttf|woff|woff2)$/,
+      use: {
+        loader: 'url-loader',
+        options: {
+          limit: 100000,
+          publicPath: './',
+          outputPath: 'static/',
+          name: '[name].[ext]',
+        },
+      },
+    });
+
     return config;
   },
-};
+});
